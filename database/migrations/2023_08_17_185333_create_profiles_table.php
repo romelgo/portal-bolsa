fe87471705d706536c184ac24e07ef049742cd08
romelgo <romelgutierrez90@gmail.com>
2024-05-08 19:48:18 -0500

    update table profiles

--- database/migrations/2023_08_17_185333_create_profiles_table.php
+++ database/migrations/2023_08_17_185333_create_profiles_table.php
@@ -15,14 +15,14 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('address')->nullable();
-            $table->string('gender');
-            $table->string('dob');
-            $table->text('experience');
-            $table->string('phone');
-            $table->text('bio');
-            $table->string('cover_letter');
-            $table->string('resume');
-            $table->string('avatar');
+            $table->string('gender')->nullable();
+            $table->string('dob')->nullable();
+            $table->text('experience')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('cover_letter')->nullable();
+            $table->string('resume')->nullable();
+            $table->string('avatar')->nullable();
             $table->timestamps();
         });
     }
