71750c680e53c03c70b67baed22a563968d61318
romelgo <romelgutierrez90@gmail.com>
2024-05-08 19:30:06 -0500

    update model

--- database/migrations/2023_08_17_185333_create_profiles_table.php
+++ database/migrations/2023_08_17_185333_create_profiles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->string('gender');
             $table->string('dob');
             $table->text('experience');
